52cff5ff2b1931485d4ddfd9689ae8f149c3ba27
jihyuk <jhbok@giantswarm.io>
2019-01-10 10:16:42 +0100

    update godoc in spec

--- spec.go
+++ spec.go
@@ -8,8 +8,8 @@ import (
 )
 
 type Interface interface {
-	// NewG8sClient returns a new generated clientset for a tenant cluster.
+	// NewG8sClientFromSecret returns a generated clientset based on the kubeconfig stored in a secret.
 	NewG8sClientFromSecret(ctx context.Context, secretName, secretNamespace string) (versioned.Interface, error)
-	// NewK8sClient returns a new Kubernetes clientset for a tenant cluster.
+	// NewK8sClientFromSecret returns a Kubernetes clientset based on the kubeconfig stored in a secret.
 	NewK8sClientFromSecret(ctx context.Context, secretName, secretNamespace string) (kubernetes.Interface, error)
 }
