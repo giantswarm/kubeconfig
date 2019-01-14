a2f7c6c3a43c3b5f9d2c54836d2ff5279e6cdf56
Jihyuk Bok <tomahawk28@users.noreply.github.com>
2019-01-14 13:24:34 +0100

    Initial structure of kubeconfig lib (#1)
    * include need vendor libs
    
    * initial impl
    
    * formatting gopkg
    
    * add micrologger on vendor dir
    
    * change all func to method
    
    * tidy up godoc
    
    * add circlici basic structure
    
    * Add README
    
    * address review #1
    
    * address review #2
    
    * change to k8s-1.13.1 constraint
    
    * update vendor
    
    * Update kubeconfig.go
    
    Co-Authored-By: tomahawk28 <tomahawk28@users.noreply.github.com>
    
    * Update kubeconfig.go
    
    Co-Authored-By: tomahawk28 <tomahawk28@users.noreply.github.com>
    
    * address review #3
    
    * update godoc in spec
    
    * Update README.md
    
    Co-Authored-By: tomahawk28 <tomahawk28@users.noreply.github.com>
    
    * review address #4
    
    * update vendor
    
    * update error statement
    
    * unittest updated
    
    * remove usage of error mask function from non-direct-pacakge calls
    
    * remove unsed test files
    
    * remove unused function
    
    * add space before headline
    
    * refactor error var name

---
+++ kubeconfig_test.go
@@ -0,0 +1,96 @@
+package kubeconfig
+
+import (
+	"testing"
+
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger/microloggertest"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes/fake"
+)
+
+func TestKubeConfig_getRESTConfigFromSecret(t *testing.T) {
+	testCases := []struct {
+		name           string
+		presentSecrets []*corev1.Secret
+		errorMatcher   func(error) bool
+	}{
+		{
+			name: "case 1: no matching secret",
+			presentSecrets: []*corev1.Secret{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Labels:    map[string]string{},
+						Name:      "kubeconfig-secret-gs-1",
+						Namespace: metav1.NamespaceNone,
+					},
+					Data: map[string][]byte{
+						"test": []byte("test"),
+					},
+				},
+			},
+			errorMatcher: errors.IsNotFound,
+		},
+		{
+			name: "case 2: no kubeconfig found",
+			presentSecrets: []*corev1.Secret{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Labels:    map[string]string{},
+						Name:      "kubeconfig-secret-gs",
+						Namespace: metav1.NamespaceNone,
+					},
+					Data: map[string][]byte{
+						"test": []byte("test"),
+					},
+				},
+			},
+			errorMatcher: IsNotFoundError,
+		},
+		{
+			name: "case 3: secret found and no error",
+			presentSecrets: []*corev1.Secret{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Labels:    map[string]string{},
+						Name:      "kubeconfig-secret-gs",
+						Namespace: metav1.NamespaceNone,
+					},
+					Data: map[string][]byte{
+						"kubeConfig": []byte("test"),
+					},
+				},
+			},
+			errorMatcher: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+
+			objs := make([]runtime.Object, 0, len(tc.presentSecrets))
+			for _, cc := range tc.presentSecrets {
+				objs = append(objs, cc)
+			}
+
+			k := KubeConfig{
+				logger:    microloggertest.New(),
+				k8sClient: fake.NewSimpleClientset(objs...),
+			}
+			_, err := k.getKubeConfigFromSecret(nil, "kubeconfig-secret-gs", "")
+
+			switch {
+			case err != nil && tc.errorMatcher == nil:
+				t.Fatalf("error == %#v, want nil", err)
+			case err == nil && tc.errorMatcher != nil:
+				t.Fatalf("error == nil, want non-nil")
+			case tc.errorMatcher != nil && !tc.errorMatcher(microerror.Cause(err)):
+				t.Fatalf("error == %#v, want matching", err)
+			}
+		})
+
+	}
+}
