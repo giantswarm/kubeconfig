1d26b8456859ab0eaee6d3f659806f6093e00526
jihyuk <jhbok@giantswarm.io>
2019-02-06 10:26:01 +0100

    address review #2

--- kubeconfig.go
+++ kubeconfig.go
@@ -11,7 +11,7 @@ import (
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	restclient "k8s.io/client-go/rest"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
@@ -55,16 +55,13 @@ func New(config Config) (*KubeConfig, error) {
 // NewG8sClientForApp returns a generated clientset for the cluster configured
 // in the kubeconfig section of the app CR. If this is empty a clientset for
 // the current cluster is returned.
-func (k KubeConfig) NewG8sClientForApp(ctx context.Context, app v1alpha1.App) (versioned.Interface, error) {
-	secretName := secretName(app)
-	secretNamespace := secretNamespace(app)
-
+func (k *KubeConfig) NewG8sClientForApp(ctx context.Context, app v1alpha1.App) (versioned.Interface, error) {
 	// KubeConfig is not configured so connect to current cluster.
-	if secretName == "" {
+	if secretName(app) != "" {
 		return k.g8sClient, nil
 	}
 
-	restConfig, err := k.NewRESTConfigForApp(ctx, secretName, secretNamespace)
+	restConfig, err := k.NewRESTConfigForApp(ctx, app)
 	if err != nil {
 		return nil, err
 	}
@@ -79,16 +76,13 @@ func (k KubeConfig) NewG8sClientForApp(ctx context.Context, app v1alpha1.App) (v
 // NewK8sClientForApp returns a Kubernetes clientset for the cluster configured
 // in the kubeconfig section of the app CR. If this is empty a clientset for
 // the current cluster is returned.
-func (k KubeConfig) NewK8sClientForApp(ctx context.Context, app v1alpha1.App) (kubernetes.Interface, error) {
-	secretName := secretName(app)
-	secretNamespace := secretNamespace(app)
-
+func (k *KubeConfig) NewK8sClientForApp(ctx context.Context, app v1alpha1.App) (kubernetes.Interface, error) {
 	// KubeConfig is not configured so connect to current cluster.
-	if secretName == "" {
+	if secretName(app) == "" {
 		return k.k8sClient, nil
 	}
 
-	restConfig, err := k.NewRESTConfigForApp(ctx, secretName, secretNamespace)
+	restConfig, err := k.NewRESTConfigForApp(ctx, app)
 	if err != nil {
 		return nil, microerror.Mask(err)
 	}
@@ -100,7 +94,10 @@ func (k KubeConfig) NewK8sClientForApp(ctx context.Context, app v1alpha1.App) (k
 	return client, nil
 }
 
-func (k KubeConfig) NewRESTConfigForApp(ctx context.Context, secretName, secretNamespace string) (*restclient.Config, error) {
+func (k *KubeConfig) NewRESTConfigForApp(ctx context.Context, app v1alpha1.App) (*rest.Config, error) {
+	secretName := secretName(app)
+	secretNamespace := secretNamespace(app)
+
 	kubeConfig, err := k.getKubeConfigFromSecret(ctx, secretName, secretNamespace)
 	if err != nil {
 		return nil, microerror.Mask(err)
@@ -114,7 +111,7 @@ func (k KubeConfig) NewRESTConfigForApp(ctx context.Context, secretName, secretN
 }
 
 // getKubeConfigFromSecret returns KubeConfig bytes based on the specified secret information.
-func (k KubeConfig) getKubeConfigFromSecret(ctx context.Context, secretName, secretNamespace string) ([]byte, error) {
+func (k *KubeConfig) getKubeConfigFromSecret(ctx context.Context, secretName, secretNamespace string) ([]byte, error) {
 	secret, err := k.k8sClient.CoreV1().Secrets(secretNamespace).Get(secretName, metav1.GetOptions{})
 	if errors.IsNotFound(err) {
 		return nil, microerror.Maskf(err, fmt.Sprintf("can't find the secretName: %#q, ns: %#q", secretName, secretNamespace), notFoundError)
