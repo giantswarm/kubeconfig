fb45df1633163291bd272b22df09199054d6e7b3
Theo Brigitte <theo.brigitte@gmail.com>
2021-03-11 17:11:24 +0100

    update Interface

--- spec.go
+++ spec.go
@@ -9,5 +9,5 @@ import (
 type Interface interface {
 	// NewRESTConfigForApp returns a Kubernetes REST Config for the cluster configured
 	// in the secrets objects.
-	NewRESTConfigForApp(ctx context.Context, secretName, secretNamespace string) (*rest.Config, error)
+	NewRESTConfigForApp(ctx context.Context, secretName, secretNamespace, secretKey string) (*rest.Config, error)
 }
